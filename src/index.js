608bb9bd2197799a0e8c39f49c08762eb9917d43
Ake Tangkananond <iamake@gmail.com>
2017-07-02 01:38:01 +0700

    Exclude some files from loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import _ from 'lodash';
+
 function App() {
   const coord = { lat: 13.7710663, lon: 100.567965 };
   const place = {
     ...coord,
     title: 'The Street Ratchada',
+    series: _.range(0, 10),
   };
 
   const elem = document.createElement('div');
